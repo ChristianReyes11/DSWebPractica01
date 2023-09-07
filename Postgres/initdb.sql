c494a72de3a84ce2ad1875ae0c0ab61b0173b8e9
YoAngelCruz <angelcruz.feb@gmail.com>
2023-09-07 07:58:50 -0600

    add telefono

--- Postgres/initdb.sql
+++ Postgres/initdb.sql
@@ -1,19 +1,16 @@
 CREATE USER myuser;
 CREATE DATABASE mydb;
 GRANT ALL PRIVILEGES ON DATABASE mydb TO myuser;
+
 ALTER DATABASE mydb OWNER TO myuser;
 
 \connect mydb
-CREATE TABLE mytable
+CREATE TABLE pejemplo
 (
-	clave integer NOT NULL,
-	nombre character varying,
-	direccion character varying,
-	CONSTRAINT pk_clave PRIMARY KEY (clave)
+    clave SERIAL PRIMARY KEY,
+    nombre character varying,
+    direccion character varying,
+    telefono character varying
 );
 
-ALTER TABLE mytable OWNER TO myuser;
-
-
---hola desde mi rama
---hola desde mi branch
+ALTER TABLE pejemplo OWNER TO myuser;
