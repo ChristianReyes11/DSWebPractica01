2fb2fb5082a944954c8c045fc56f7b0a50b2d451
IvanAVila1224 <magista1224@gmail.com>
2023-09-11 22:31:26 -0600

    validations created

--- PHP-Apache/src/index.php
+++ PHP-Apache/src/index.php
@@ -2,49 +2,62 @@
 $url = "pgsql:host=172.17.0.2;port=5432;dbname=mydb;";
 $pdo = new PDO($url, "postgres", "password", [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
 
+$errors = [];
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    try {
-        $nombre = $_POST['nombre'];
-        $direccion = $_POST['direccion'];
-        $telefono = $_POST['telefono'];
+    $nombre = $_POST['nombre'];
+    $direccion = $_POST['direccion'];
+    $telefono = $_POST['telefono'];
 
-        if (isset($_POST['submitInsert']) ) {
-//INSERT
-            $insert = $pdo->prepare("INSERT INTO mytable (nombre, direccion, telefono) VALUES (:nombre, :direccion, :telefono)");
-            $insert->bindParam(':nombre', $nombre);
-            $insert->bindParam(':direccion', $direccion);
-            $insert->bindParam(':telefono', $telefono);
-            $insert->execute();
-        } elseif (isset($_POST['submitUpdate'])) {
-//UPDATE
-            $idEditar = $_POST['idEditar'];
-
-            $verificarExistencia = $pdo->prepare("SELECT COUNT(*) FROM mytable WHERE clave = :id");
-            $verificarExistencia->bindParam(':id', $idEditar);
-            $verificarExistencia->execute();
-
-            if ($verificarExistencia->fetchColumn() > 0) {
-
-            
-            $update = $pdo->prepare("UPDATE mytable SET nombre = :nombre, direccion = :direccion, telefono = :telefono WHERE clave = :clave");
-            $update->bindParam(':nombre', $nombre);
-            $update->bindParam(':direccion', $direccion);
-            $update->bindParam(':telefono', $telefono);
-            $update->bindParam(':clave', $idEditar);
-            $update->execute();
-            } else {
-
-            echo 'El registro a editar no existe.';
+    if (isset($_POST['submitInsert']) || isset($_POST['submitUpdate'])) {
+        if (empty($nombre)) {
+            $errors[] = "El campo Nombre es obligatorio.";
         }
+        if (empty($direccion)) {
+            $errors[] = "El campo Dirección es obligatorio.";
+        }
+        if (empty($telefono)) {
+            $errors[] = "El campo Teléfono es obligatorio.";
         }
 
-        header('Location: index.php');
-        exit();
-    } catch (PDOException $e) {
-        die($e->getMessage());
+        if (empty($errors)) {
+            try {
+                if (isset($_POST['submitInsert'])) {
+                    // INSERT
+                    $insert = $pdo->prepare("INSERT INTO mytable (nombre, direccion, telefono) VALUES (:nombre, :direccion, :telefono)");
+                    $insert->bindParam(':nombre', $nombre);
+                    $insert->bindParam(':direccion', $direccion);
+                    $insert->bindParam(':telefono', $telefono);
+                    $insert->execute();
+                } elseif (isset($_POST['submitUpdate'])) {
+                    // UPDATE
+                    $idEditar = $_POST['idEditar'];
+
+                    $verificarExistencia = $pdo->prepare("SELECT COUNT(*) FROM mytable WHERE clave = :id");
+                    $verificarExistencia->bindParam(':id', $idEditar);
+                    $verificarExistencia->execute();
+
+                    if ($verificarExistencia->fetchColumn() > 0) {
+                        $update = $pdo->prepare("UPDATE mytable SET nombre = :nombre, direccion = :direccion, telefono = :telefono WHERE clave = :clave");
+                        $update->bindParam(':nombre', $nombre);
+                        $update->bindParam(':direccion', $direccion);
+                        $update->bindParam(':telefono', $telefono);
+                        $update->bindParam(':clave', $idEditar);
+                        $update->execute();
+                    } else {
+                        echo 'El registro a editar no existe.';
+                    }
+                }
+
+                header('Location: index.php');
+                exit();
+            } catch (PDOException $e) {
+                die($e->getMessage());
+            }
+        }
     }
 }
-//SELECT para editar los campos
+
 if (isset($_GET['editar'])) {
     $idEditar = $_GET['editar'];
     $selectById = $pdo->prepare("SELECT * FROM mytable WHERE clave = :clave");
@@ -59,7 +72,6 @@ if (isset($_GET['editar'])) {
     }
 }
 
-//DELETE 
 if (isset($_GET['eliminar'])) {
     $idToDelete = $_GET['eliminar'];
 
@@ -76,23 +88,42 @@ if (isset($_GET['eliminar'])) {
     <meta charset="UTF-8" /> 
     <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
     <title>REGISTRO</title>
+    
+    <script>
+        function confirmarEliminacion() {
+            return confirm('¿Estás seguro de que deseas eliminar este registro?');
+        }
+
+        function validarFormulario() {
+            var nombre = document.getElementsByName("nombre")[0].value;
+            var direccion = document.getElementsByName("direccion")[0].value;
+            var telefono = document.getElementsByName("telefono")[0].value;
+
+            if (nombre.trim() === "" || direccion.trim() === "" || telefono.trim() === "") {
+                alert("Por favor, completa todos los campos.");
+                return false;
+            }
+
+            return true;
+        }
+    </script>
 </head>
 <body>
-    <form action="index.php" method="POST" autocomplete="off">
+    <form action="index.php" method="POST" autocomplete="off" onsubmit="return validarFormulario();">
         <h1>INGRESA LOS DATOS DEL FORMULARIO</h1>
         <div>
             <table>
                 <tr>
                     <td>Nombre: </td>
-                    <td><input class="input" type="text" name="nombre" value="<?php echo isset($nombre) ? $nombre : ''; ?>" required/></td>
+                    <td><input class="input" type="text" name="nombre" value="<?php echo isset($nombre) ? $nombre : ''; ?>" /></td>
                 </tr>
                 <tr>
                     <td>Direccion: </td>
-                    <td><input class="input" type="text" name="direccion" value="<?php echo isset($direccion) ? $direccion : ''; ?>" required/></td>
+                    <td><input class="input" type="text" name="direccion" value="<?php echo isset($direccion) ? $direccion : ''; ?>" /></td>
                 </tr> 
                 <tr>
                     <td>Telefono: </td>
-                    <td><input class="input" type="text" name="telefono" value="<?php echo isset($telefono) ? $telefono : ''; ?>" required/></td>
+                    <td><input class="input" type="text" name="telefono" value="<?php echo isset($telefono) ? $telefono : ''; ?>" /></td>
                 </tr>
             </table>
         </div>
@@ -102,55 +133,64 @@ if (isset($_GET['eliminar'])) {
     </form>
 
     <?php
-//MOSTRAR TABLA
-        if ($pdo) {
-            $select = $pdo->prepare("SELECT * FROM mytable");
-            $select->execute();
-            $result = $select->fetchAll(PDO::FETCH_ASSOC);
+    if (!empty($errors)) {
+        echo '<div class="errors"><ul>';
+        foreach ($errors as $error) {
+            echo '<li>' . $error . '</li>';
+        }
+        echo '</ul></div>';
+    }
     
-            if (count($result) > 0) {
-                echo '<h2>Resultados:</h2>';
-                echo '<table border="1">';
-                echo '<tr>
-                        <th> Clave </th>
-                        <th> Nombre </th>
-                        <th> Direccion </th>
-                        <th> Telefono </th>
-                        <th> Actualizar </th>
-                        <th> Eliminar </th> </tr>';
-                
-                foreach ($result as $row) {
-                    echo '<tr>';
-                    echo '<td><a href="index.php?clave=' . $row['clave'] . '">' . $row['clave'] . '</a></td>';
-                    echo '<td>' . $row['nombre'] . '</td>';
-                    echo '<td>' . $row['direccion'] . '</td>';
-                    echo '<td>' . $row['telefono'] . '</td>';
-                    echo '<td><a href="index.php?editar=' . $row['clave'] . '">Editar</a></td>';
-                    echo '<td><a href="index.php?eliminar=' . $row['clave'] . '">Eliminar</a></td>';
-    
-                    echo '</tr>';
-                }
-                
-                echo '</table>';
-            }
-//Search by ID
-            if (isset($_GET['clave'])) {
-                $idSeleccionado = $_GET['clave'];
-    
-                $selectById = $pdo->prepare("SELECT * FROM mytable WHERE clave = :id");
-                $selectById->bindParam(':id', $idSeleccionado);
-                $selectById->execute();
-                $registroSeleccionado = $selectById->fetch(PDO::FETCH_ASSOC);
+    if ($pdo) {
+        $select = $pdo->prepare("SELECT * FROM mytable");
+        $select->execute();
+        $result = $select->fetchAll(PDO::FETCH_ASSOC);
 
-                if ($registroSeleccionado) {
-                    echo '<h2>Detalles del Registro Seleccionado:</h2>';
-                    echo 'Clave: ' . $registroSeleccionado['clave'] . '<br>';
-                    echo 'Nombre: ' . $registroSeleccionado['nombre'] . '<br>';
-                    echo 'Direccion: ' . $registroSeleccionado['direccion'] . '<br>';
-                    echo 'Telefono: ' . $registroSeleccionado['telefono'] . '<br>';
-                }
+        if (count($result) > 0) {
+            echo '<h2>Resultados:</h2>';
+            echo '<table border="1">';
+            echo '<tr>
+                    <th> Clave </th>
+                    <th> Nombre </th>
+                    <th> Direccion </th>
+                    <th> Telefono </th>
+                    <th> Actualizar </th>
+                    <th> Eliminar </th> </tr>';
+
+            foreach ($result as $row) {
+                echo '<tr>';
+                echo '<td><a href="index.php?clave=' . $row['clave'] . '">' . $row['clave'] . '</a></td>';
+                echo '<td>' . $row['nombre'] . '</td>';
+                echo '<td>' . $row['direccion'] . '</td>';
+                echo '<td>' . $row['telefono'] . '</td>';
+                echo '<td><a href="index.php?editar=' . $row['clave'] . '">Editar</a></td>';
+
+                // Agrega la confirmación de eliminación aquí
+                echo '<td><a href="index.php?eliminar=' . $row['clave'] . '" onclick="return confirmarEliminacion();">Eliminar</a></td>';
+
+                echo '</tr>';
+            }
+
+            echo '</table>';
+        }
+
+        if (isset($_GET['clave'])) {
+            $idSeleccionado = $_GET['clave'];
+
+            $selectById = $pdo->prepare("SELECT * FROM mytable WHERE clave = :id");
+            $selectById->bindParam(':id', $idSeleccionado);
+            $selectById->execute();
+            $registroSeleccionado = $selectById->fetch(PDO::FETCH_ASSOC);
+
+            if ($registroSeleccionado) {
+                echo '<h2>Detalles del Registro Seleccionado:</h2>';
+                echo 'Clave: ' . $registroSeleccionado['clave'] . '<br>';
+                echo 'Nombre: ' . $registroSeleccionado['nombre'] . '<br>';
+                echo 'Direccion: ' . $registroSeleccionado['direccion'] . '<br>';
+                echo 'Telefono: ' . $registroSeleccionado['telefono'] . '<br>';
             }
         }
+    }
     ?>
 </body>
 </html>
